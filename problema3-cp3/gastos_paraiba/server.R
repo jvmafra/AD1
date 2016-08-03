ffafe61c2ce31214e58c0aaca5d159a76920e9c5
jvmafra <joao.mafra@ccc.ufcg.edu.br>
2016-08-03 00:21:24 -0300

    up cp3

--- problema3-cp3/gastos_paraiba/server.R
+++ problema3-cp3/gastos_paraiba/server.R
@@ -15,6 +15,8 @@ gastos <- ler_gastos("../dados/ano-atual.csv")
 gastos = gastos %>% 
   mutate_each(funs(as.factor), sgPartido, sgUF, txNomeParlamentar, indTipoDocumento)
 
+gastos[gastos$txtDescricao == "Emissão Bilhete Aéreo", "txtDescricao"] = "PASSAGENS AÉREAS"
+
 posicao <- ler_gastos("../dados/posicao.csv")
 
 gastos <- merge(gastos, posicao)
@@ -28,19 +30,40 @@ gastos_pb <- gastos_pb %>% filter (Mes != 7)
 gastos_pb <- gastos_pb %>% filter (Mes != 6)
 
 
+teste <- gastos_pb %>% group_by(Tipo) %>% summarise(valor = sum(Valor))
+
+
 meses <- list("Janeiro" = 1,"Fevereiro" = 2, "Março" = 3, "Abril" = 4, "Maio" = 5)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
   output$plotMes <- renderPlot({
+    if (input$tipo != "TODOS"){
+      gastos_pb <- gastos_pb %>% filter (Tipo == input$tipo)
+      nome.tipo = input$tipo
+      if (input$tipo == "DIVULGAÇÃO DA ATIVIDADE PARLAMENTAR."){
+        if (input$mes == "Todos") escala <- c(0, 150000)
+        else escala <- c(0, 60000)
+      } 
+      else escala <- c(0, 50000)
+      
+      
+      tipo_gasto = TRUE
+      
+    } else {
+      nome.tipo = "Todos os tipos de gastos"
+      tipo_gasto = FALSE
+    }
+    
     if (input$deputado == "TODOS"){
       if (input$mes != "Todos"){
         gastos_pb <- gastos_pb %>% filter (Mes == meses[input$mes])
-        escala <- c(0, 70000)
+        if (tipo_gasto == FALSE) escala <- c(0, 70000)
+        
         nome.mes <- input$mes
       } else {
-        escala <- c(0, 250000)
+        if (tipo_gasto == FALSE) escala <- c(0, 250000)
         nome.mes <- "Janeiro a Maio"
       }
       
@@ -50,20 +73,23 @@ shinyServer(function(input, output) {
       
       ggplot(agrupa_nome, aes(x = reorder(sprintf("%s - %s", Nome, Partido), total_gasto),  
                               y = total_gasto,  
-                              fill = Ideologia_Partidaria)) + 
+                              fill = Ideologia_Partidaria)) +
+        ggtitle(sprintf("Gastos dos deputados (%s)", nome.mes)) +
         geom_bar(stat = "summary", fun.y = "mean") + 
         scale_y_continuous(limits = escala) +
         coord_flip() + 
         xlab("Nome do deputado") +
-        ylab(sprintf("Valor gasto (%s)", nome.mes)) +
+        ylab("Valor gasto") +
         labs(fill = "Ideologia partidária")
       
     } else {
       gastos_deputado <- gastos_pb %>% filter(Nome == input$deputado)
       agrupa_mes <- gastos_deputado %>% group_by(Mes) %>% summarise(valor = sum(Valor))
       
-      ggplot(agrupa_mes) + ggtitle(sprintf("GASTOS (JANEIRO A MAIO) DE %s", input$deputado)) +
-        geom_point(aes(x = Mes, y = valor), colour = "deepskyblue3", size = 4) +
+      ggplot(agrupa_mes, aes(x = Mes, y = valor, group = 1)) +
+        ggtitle(sprintf("GASTOS (JANEIRO A MAIO) DE %s", input$deputado)) +
+        geom_line(colour = "dodgerblue4", alpha=.7) +
+        geom_point(colour = "deepskyblue3", size = 3) +
         scale_x_continuous(limits = c(1,5)) +
         scale_y_continuous(limits = c(0, 70000))
 
--- problema3-cp3/gastos_paraiba/ui.R
+++ problema3-cp3/gastos_paraiba/ui.R
@@ -7,31 +7,43 @@ shinyUI(fluidPage(
   # Application title
   titlePanel("Gastos dos deputados paraibanos em 2016"),
   
+  helpText("Nota: Quando algum deputado está selecionado, o filtro do mês torna-se irrelevante,
+           tendo em vista que já está sendo mostrada a evolução dos gastos daquele deputado mês
+           a mês"),
+  
   # Sidebar with a slider input for number of bins 
-  pageWithSidebar(
+  sidebarLayout(
     sidebarPanel(
       radioButtons("mes", 
                   label = "Escolha um mês",
-                  choices = c("Janeiro", "Fevereiro",
+                  list("Janeiro", "Fevereiro",
                               "Março", "Abril", "Maio", "Todos"),
                   selected = "Janeiro",
-                  inline = TRUE, width = '400px')
-    ),
-    
-    sidebarPanel(
+                  inline = FALSE, width = '400px'),
+      selectInput("tipo",
+                  label = "Tipo de gasto",
+                  choices = c("ASSINATURA DE PUBLICAÇÕES","COMBUSTÍVEIS E LUBRIFICANTES.",
+                              "CONSULTORIAS, PESQUISAS E TRABALHOS TÉCNICOS.",
+                              "DIVULGAÇÃO DA ATIVIDADE PARLAMENTAR.", "FORNECIMENTO DE ALIMENTAÇÃO DO PARLAMENTAR",
+                              "HOSPEDAGEM ,EXCETO DO PARLAMENTAR NO DISTRITO FEDERAL", "LOCAÇÃO OU FRETAMENTO DE VEÍCULOS AUTOMOTORES",
+                              "MANUTENÇÃO DE ESCRITÓRIO DE APOIO À ATIVIDADE PARLAMENTAR",
+                              "PASSAGENS AÉREAS",
+                              "SERVIÇO DE SEGURANÇA PRESTADO POR EMPRESA ESPECIALIZADA.", "SERVIÇO DE TÁXI, PEDÁGIO E ESTACIONAMENTO",
+                              "SERVIÇOS POSTAIS","TELEFONIA","TODOS"),
+                  selected = "TODOS"),
       selectInput("deputado", 
-                   label = "Escolha um deputado",
-                   choices = c("AGUINALDO RIBEIRO", "BENJAMIN MARANHÃO", "DAMIÃO FELICIANO",
-                               "EFRAIM FILHO", "HUGO MOTTA", "LUIZ COUTO", "MANOEL JUNIOR",
-                               "MARCONDES GADELHA", "PEDRO CUNHA LIMA", "RÔMULO GOUVEIA",
-                               "VENEZIANO VITAL DO RÊGO", "WELLINGTON ROBERTO", "WILSON FILHO",
-                               "TODOS"),
-                   selected = "TODOS")
+                  label = "Evolução dos gastos por deputado",
+                  choices = c("AGUINALDO RIBEIRO", "BENJAMIN MARANHÃO", "DAMIÃO FELICIANO",
+                              "EFRAIM FILHO", "HUGO MOTTA", "LUIZ COUTO", "MANOEL JUNIOR",
+                              "MARCONDES GADELHA", "PEDRO CUNHA LIMA", "RÔMULO GOUVEIA",
+                              "VENEZIANO VITAL DO RÊGO", "WELLINGTON ROBERTO", "WILSON FILHO",
+                              "TODOS"),
+                  selected = "TODOS")
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("plotMes")
+      plotOutput("plotMes", height = "550px")
     )
   )
 ))
